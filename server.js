0c20867c14b7fe2a4eddc6487d0b3530526b0060
Denis ZHENG <zheng.denis@gmail.com>
2018-10-03 20:55:03 +0200

    Class for server

--- server.js
+++ server.js
@@ -2,44 +2,69 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs } = require('./graphql/schema');
 const { resolvers } = require('./graphql/resolvers');
-
 const fs = require('fs');
 const https = require('https');
 const http = require('http');
+
 const config = require(process.env.NODE_ENV === 'production'
-  ? './config/config.prod'
-  : './config/config.dev').config;
+	? './config/config.prod'
+	: './config/config.dev').config;
 
-const apollo = new ApolloServer({ typeDefs, resolvers });
+class Server {
+	constructor() {
+		this.app = express();
+		this.apollo = new ApolloServer({ typeDefs, resolvers });
+		this.server = this.configServer();
+		
+		this.apolloMiddleware();
+		this.pubSup();
+		this.initRoutes();
+		this.start();
+	}
 
-const app = express();
-apollo.applyMiddleware({ app });
+	apolloMiddleware() {
+		this.apollo.applyMiddleware({ app: this.app });
+	}
 
-//  Create the HTTPS or HTTP server, per configuration
-var server;
-if (config.ssl) {
-  // Assumes certificates are in .ssl folder from package root. Make sure the files
-  // are secured.
-  server = https.createServer(
-    {
-      key: fs.readFileSync(`./ssl/${process.env.NODE_ENV}/server.key`),
-      cert: fs.readFileSync(`./ssl/${process.env.NODE_ENV}/server.crt`),
-    },
-    app
-  );
-} else {
-  server = http.createServer(app);
+	configServer() {
+		//  Create the HTTPS or HTTP server, per configuration
+		let server;
+		if (config.ssl) {
+			// Assumes certificates are in .ssl folder from package root. Make sure the files
+			// are secured.
+			server = https.createServer(
+				{
+					key: fs.readFileSync(`./ssl/${process.env.NODE_ENV}/server.key`),
+					cert: fs.readFileSync(`./ssl/${process.env.NODE_ENV}/server.crt`),
+				},
+				this.app
+			);
+		} else {
+			server = http.createServer(this.app);
+		}
+		return server;
+	}
+
+	pubSup() {
+		// Add subscription support
+		// https://github.com/davidyaha/graphql-redis-subscriptions
+		this.apollo.installSubscriptionHandlers(this.server);
+	}
+
+	initRoutes() {
+		this.app.get('/', (req, res) => {
+			res.send('Hello graphql api');
+		})
+	}
+
+	start() {
+		this.app.listen({ port: config.port }, () => {
+			console.log(
+				'🚀 Server ready at',
+				`http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${this.apollo.graphqlPath}`
+			);
+		});
+	}
 }
 
-// Add subscription support
-// https://github.com/davidyaha/graphql-redis-subscriptions
-apollo.installSubscriptionHandlers(server);
-
-app.listen({ port: config.port }, () => {
-  console.log(
-    '🚀 Server ready at',
-    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${
-      apollo.graphqlPath
-    }`
-  );
-});
+new Server();
