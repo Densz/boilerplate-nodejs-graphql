b3ce0fb8dc96f9f182ca614efe2c22f17d38512a
Denis ZHENG <zheng.denis@gmail.com>
2018-12-31 12:13:50 +0100

    [docker] Wait for db updated

--- wait-for.sh
+++ wait-for.sh
@@ -7,10 +7,10 @@ host="$1"
 shift
 cmd="$@"
 # psql -U <username> -d <database> -h <hostname>
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "root" -d "boilerplate" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U $POSTGRES_USER -d $POSTGRES_DB -c '\q'; do
+  >&2 echo "Postgres - $POSTGRES_DB is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
+>&2 echo "Postgres - $POSTGRES_DB is up - executing command"
 exec $cmd
